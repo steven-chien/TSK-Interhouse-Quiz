b780aed9b808a381ef307fcdcda291a6d909fb69
Steven <steven.chien@connect.polyu.hk>
2013-12-28 02:29:43 +0800

    modified:   buzzer_src/buzzer.c 	modified:   ipc_scoring/change_log.txt

--- buzzer_src/buzzer.c
+++ buzzer_src/buzzer.c
@@ -21,22 +21,22 @@ int main(int argc, char *argv[])
 	serv_addr.sin_port = htons(8888);
 	inet_pton(AF_INET, argv[1], &serv_addr.sin_addr);
 	connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-
+/*
 	sock1 = socket(AF_INET, SOCK_STREAM, 0);
 	memset(&serv_addr1, '0', sizeof(serv_addr1));
 	serv_addr1.sin_family = AF_INET;
 	serv_addr1.sin_port = htons(8889);
 	inet_pton(AF_INET, argv[2], &serv_addr1.sin_addr);
 	connect(sock1, (struct sockaddr*)&serv_addr1, sizeof(serv_addr1));
-
+*/
 	while((n = read(sock, recvBuff, sizeof(recvBuff)-1))>0) {
 		printf("n=%d\n", n);
 		recvBuff[strlen(recvBuff)] = 0;
 		printf("%s\n", recvBuff);
 		buff = atoi(recvBuff);
-		if((n=write(sock1, recvBuff, sizeof(recvBuff-1)))<0) {
+/*		if((n=write(sock1, recvBuff, sizeof(recvBuff-1)))<0) {
 			printf("error\n");
-		}
+		}*/
 		printf("button %s pressed and printed\n", recvBuff);
 		memset(recvBuff, 0, sizeof(recvBuff));
 	}
