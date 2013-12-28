d080643f568e689aca0ba1e9f371d03ec8c533cc
Steven <steven.chien@connect.polyu.hk>
2013-12-29 01:21:41 +0800

    fixed bug of when buttons are press in short interval of time, signals are received mixed and produce error
    bug fixed by creating a for loop to process each and every character in the receive buffer before next read, assuming that they arrive in that order.
    	deleted:    Score.cpp
    	modified:   buzzer_src/buzzer
    	modified:   buzzer_src/buzzer.c
    	new file:   control.cpp
    	modified:   ipc_scoring/score
    	new file:   score.h

---
+++ control.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <stdlib>
+#include <unistd.h>
+#include <ctype>
+#include "buzzer.h"
+#include "score.h"
+
+using namespace std;
---
+++ score.h
@@ -0,0 +1,6 @@
+#ifndef __X_H_INCLUDED__
+#define __X_H_INCLUDED__
+
+
+
+#endif
