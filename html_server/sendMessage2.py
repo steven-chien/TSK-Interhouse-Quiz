de440b585f51fa05f4efa089bb3b1650110d7c48
oneonestar <oneonestar@gmail.com>
2014-01-01 15:00:17 +0800

    command terminated using \n instead of \0
    JQuery added.
    Update directory listing method.

--- html_server/sendMessage2.py
+++ html_server/sendMessage2.py
@@ -5,7 +5,7 @@ HOST = '127.0.0.1'
 PORT=8889
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST,PORT))
-s.sendall("score:B:40\0score:A:40\0")
+s.sendall("score:B:40\nscore:A:40\n")
 #s.sendall("""question:{ "Question": "question", "Options": [ "A", "B", "C", "D" ], "Answer": "correct", "Path": "\/path\/\nabc" }\0""")
 #time.sleep(3)
 #s.sendall(":10\0")
