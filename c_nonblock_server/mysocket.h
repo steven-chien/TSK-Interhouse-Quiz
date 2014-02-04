40445f99ef30b547a470311dc091d87d72ff4239
oneonestar <oneonestar@gmail.com>
2014-02-04 21:09:04 +0800

    main server update 	modified:   my_c_server 	modified:   my_c_server.c 	modified:   mysocket.c 	modified:   mysocket.h

--- c_nonblock_server/mysocket.h
+++ c_nonblock_server/mysocket.h
@@ -4,7 +4,10 @@
 #define TRUE             1
 #define FALSE            0
 
-typedef void (*mycallback)(int fd, char* msg);
+typedef void (*conn_callback)(int fd, char* msg);
+typedef void (*timeout_callback)();
 int create_socket( int port );
-int poll_loop( int port , mycallback callbacks);
+//Timeout base on millisecond
+int poll_loop( int port , conn_callback conn_cb, timeout_callback timeout_cb, int timeout);
+int send_to_port(int port, char* msg);
 #endif
