4f6ee96d8fa50e82410b250ca85c6c23d0106c2d
Steven <steven.chien@connect.polyu.hk>
2014-02-07 18:32:59 +0800

    modified non blocking socket service to libevent2 and integrated to main 	modified:   main/makefile 	new file:   main/src/.non_blocking_socket.c.swp 	new file:   main/src/.non_blocking_socket.h.swp 	new file:   main/src/.quiz_server.c.swp 	new file:   main/src/non_blocking_socket.c 	new file:   main/src/non_blocking_socket.h 	modified:   main/src/quiz_server.c

---
+++ main/src/non_blocking_socket.h
@@ -0,0 +1,11 @@
+#ifndef _NON_BLOCKING_SOCKET_H_
+#define _NON_BLOCKING_SOCKET_H_
+#include <event2/buffer.h>
+#include <event2/bufferevent.h>
+#include <event2/listener.h>
+
+static void echo_event_cb(struct bufferevent*, short, void*);
+void read_instruct(struct bufferevent*, void*);
+void accept_connection(struct evconnlistener*, evutil_socket_t, struct sockaddr*, int, void*);
+
+#endif
