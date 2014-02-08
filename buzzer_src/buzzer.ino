52f4b68645ed889a53a31668735b7b726effb530
Steven <steven.chien@connect.polyu.hk>
2014-02-09 05:25:05 +0800

    Altered Arduino buzzer code

--- buzzer_src/buzzer.ino
+++ buzzer_src/buzzer.ino
@@ -4,6 +4,8 @@
 const int one =1;
 int counter[6]={0, 0, 0, 0, 0, 0};
 int signal = 0;//it determines which button has been pressed
+int buzzerState = 0;
+unsigned long timer[6];
 
 void setup()
 {
@@ -20,11 +22,17 @@ void setup()
   
   //setup the output pins
   pinMode(8, OUTPUT);
+  pinMode(8, LOW);
   pinMode(9, OUTPUT);
+  pinMode(9, LOW);
   pinMode(10, OUTPUT);
+  pinMode(10, LOW);
   pinMode(11, OUTPUT);
+  pinMode(11, LOW);
   pinMode(12, OUTPUT);
+  pinMode(12, LOW);
   pinMode(13, OUTPUT);
+  pinMode(13, LOW);
   
   //start i2c connection
   Wire.begin(SLAVE_ADDRESS);
@@ -34,57 +42,85 @@ void setup()
 
 void loop()
 {
-  int temp;//use for storing a temp integer for the other usage
-  signal = 0;//it determines which button has been pressed
-  
-  for(int index=2; index<8; index++)
+  while(buzzerState==1)
   {
-    //read from button
-    int buttonvoltage = digitalRead(index);
-  
-    if (buttonvoltage == HIGH)
+    //int temp;//use for storing a temp integer for the other usage
+    //signal = 0;//it determines which button has been pressed
+    
+    for(int index=2; index<8; index++)
     {
-      //digitalWrite(13, LOW);
+      if(counter[index-2]==1) {
+        continue;
+      }
+      else {
+        //read from button
+        int buttonvoltage = digitalRead(index);
+        timer[index-2] = millis();
+        if (buttonvoltage == LOW)
+        {
+          //digitalWrite(13, HIGH);
+          signal += (one << (index-2));  //rotate the bits
+          //signal += temp;  //set respective bit to 1
+          
+         // if(index==2)
+          counter[index-2]=1;// counter fot that partiucular button
+          pinMode(index+6, HIGH);
+          //Serial.println(counter[index-2]);
+        }
+      }
     }
-    else
+    //Serial.println(signal);
+/*    
+    //try to turn on the led for few seconds after pressing the button
+    for(int index = 0; index<6; index++)
     {
-      //digitalWrite(13, HIGH);
-      temp = one << (index-2);  //rotate the bits
-      signal += temp;  //set respective bit to 1
-      
-     // if(index==2)
-      counter[index-2]=1000;// counter fot that partiucular button
+      if(counter[index] !=0)
+      {
+        digitalWrite(index+8, LOW);
+        counter[index] --;
+      }
+      else
+      {
+        digitalWrite(index+8, HIGH);
+      }
     }
+*/
   }
-  Serial.println(signal);
-  
-  //if anyone presses the button, the signal should not be zero, so the led light will turn on when it happens
-  /*if(signal != 0)
-  {
-    digitalWrite(13, HIGH);
-  }
-  else
-  {
-    digitalWrite(13, LOW);
+}
+
+void reset()
+{
+  signal = 0;
+  /*
+  for(int i=0; i<6; i++) {
+    Serial.println(timer[i]);
   }
   */
-  //try to turn on the led for few seconds after pressing the button
-  for(int index = 0; index<6; index++)
-  {
-    if(counter[index] !=0)
-    {
-      digitalWrite(index+8, LOW);
-      counter[index] --;
-    }
-    else
-    {
-      digitalWrite(index+8, HIGH);
-    }
+  for(int i=0; i<6; i++) {
+    counter[i] = 0;
+    timer[i] = 0;
+    pinMode(i+8, LOW);
   }
 }
 
 void receiveData(int byteCount) {
+  while(Wire.available()) {
+    int reader = Wire.read();
+    
+    if(reader==0) {
+      buzzerState = reader;
+      reset();
+    }
+    else {
+      reset();
+      buzzerState = reader;
+    }
   
+    Serial.print("data received: ");
+    Serial.println(buzzerState);
+    //Serial.print("data size: ");
+    //Serial.println(byteCount);
+  }
 }
 
 void sendData() {
