6c93860dc24be88f94657e8ae66cf57f4124550a
Steven <steven.chien@connect.polyu.hk>
2014-11-30 03:07:13 +0800

    Migrated basic display of score and question answer to Meteor.
    Converted score and question panels into templates. Added Login package and UI.
    
    	new file:   interhouse-quiz-display/.meteor/cordova-plugins
    	modified:   interhouse-quiz-display/.meteor/packages
    	modified:   interhouse-quiz-display/.meteor/versions
    	modified:   interhouse-quiz-display/interhouse-quiz-display.html
    	modified:   interhouse-quiz-display/interhouse-quiz-display.js

--- interhouse-quiz-display/interhouse-quiz-display.js
+++ interhouse-quiz-display/interhouse-quiz-display.js
@@ -1,23 +1,60 @@
+Scores = new Mongo.Collection('scores');
+Questions = new Mongo.Collection('questions');
+Answers = new Mongo.Collection('answers');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
+	Meteor.subscribe('theScores');
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
+	Template.score_board.helpers({
+		'A_score': function() {
+			return Scores.findOne({ house: 'A' });
+		},
+		'D_score': function() {
+			return Scores.findOne({ house: 'D' });
+		},
+		'H_score': function() {
+			return Scores.findOne({ house: 'H' });
+		},
+		'J_score': function() {
+			return Scores.findOne({ house: 'J' });
+		},
+		'L_score': function() {
+			return Scores.findOne({ house: 'L' });
+		},
+		'M_score': function() {
+			return Scores.findOne({ house: 'M' });
+		}
+	});
+
+	Template.question_board.helpers({
+		'question': function() {
+			return Questions.findOne();
+		},
+		'answer': function() {
+			return Answers.findOne();
+		}
+	});
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+	Meteor.startup(function () {
+		// code to run on server at startup
+
+		// initialize scores
+		Scores.insert({ house: 'A', score: 0 });
+		Scores.insert({ house: 'D', score: 0 });
+		Scores.insert({ house: 'H', score: 0 });
+		Scores.insert({ house: 'J', score: 0 });
+		Scores.insert({ house: 'L', score: 0 });
+		Scores.insert({ house: 'M', score: 0 });
+
+		// initialze questions
+		Questions.insert({ catalog: "catalog", Id: 1, content: "content", image: "" });
+		Answers.insert({ optionA: "Option A", optionB: "Option B", optionC: "Option C", optionD: "Option D", correct: "Correct Answer" });
+	});
+
+	Meteor.publish('theScores', function() {
+		return Scores.find({},{ house: 1, score: 1 });
+	});
 }
