87c44a23c049c0aa5268437a4a97f178aed86667
Steven <steven.chien@connect.polyu.hk>
2015-01-17 01:26:40 +0800

    Implemented functions using MongoDB C API to update web server. Currently only function for score update is implemented.

---
+++ main/src/include/webserver_connector.h
@@ -0,0 +1,10 @@
+#ifndef _WEBSERVER_CONNECTOR_H_
+#define _WEBSERVER_CONNECTOR_H_
+
+char address_str[100];
+char collection_str[50];
+
+void webserver_update_score(char*, int);
+void webserver_init(char*, char*);
+
+#endif
---
+++ main/src/webserver_connector.c
@@ -0,0 +1,62 @@
+#include <bcon.h>
+#include <bson.h>
+#include <stdio.h>
+#include <mongoc.h>
+#include "include/layout.h"	/* <-ncurses.h */
+#include "include/webserver_connector.h"
+
+void log_handler(mongoc_log_level_t log_level, const char *log_domain, const char *message, void *user_data)
+{
+
+}
+
+void webserver_init(char *host, char *port)
+{
+	mongoc_log_set_handler(log_handler, NULL);
+	sprintf(address_str, "mongodb://%s:%s/", "127.0.0.1", "3001");
+	sprintf(collection_str, "%s", "scores");
+}
+
+void webserver_update_score(char *name, int new_score)
+{
+	mongoc_collection_t *collection;
+	mongoc_client_t *client;
+	bson_error_t error;
+	bson_t *doc = NULL;
+	bson_t *update = NULL;
+	bson_t *query = NULL;
+
+	mongoc_init ();
+
+	client = mongoc_client_new(address_str);
+	collection = mongoc_client_get_collection(client, "meteor", collection_str);
+
+	query = BCON_NEW ("house", BCON_UTF8(name));
+	update = BCON_NEW ("$set", "{",
+		"score", BCON_INT32 (new_score),
+		"}");
+
+	if (!mongoc_collection_update (collection, MONGOC_UPDATE_NONE, query, update, NULL, &error)) {
+		char *bson_str = bson_as_json(doc, NULL);
+		wprintw(msg_content, "Web server push failed: %s, %s\n", error.message,bson_str);
+		bson_destroy(doc);
+		free(bson_str);
+	}
+
+	mongoc_collection_destroy (collection);
+	mongoc_client_destroy (client);
+}
+
+/*
+int main(int argc, char *argv[]) {
+
+	memset(&address_str, 0, sizeof(address_str));
+	sprintf(address_str, "mongodb://%s:%s/", argv[1], argv[2]);
+	sprintf(collection_str, "scores");
+	printf("%s\n", address_str);
+	printf("%s: %d\n", argv[3], atoi(argv[4]));
+
+	update_webserver_score(argv[3], atoi(argv[4]));
+	return 0;
+}
+*/
