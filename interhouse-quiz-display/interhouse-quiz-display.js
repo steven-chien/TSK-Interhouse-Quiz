2a9c22ce938ce0deafaee4ed73cf4d8d92bf0703
Steven <steven.chien@connect.polyu.hk>
2015-01-31 01:25:36 +0800

    Implemented work around relational join of mongo db during publish and subscribe.

--- interhouse-quiz-display/interhouse-quiz-display.js
+++ interhouse-quiz-display/interhouse-quiz-display.js
@@ -1,14 +1,25 @@
+/*
+ * Scores-> storing score of 6 houses
+ * Questions-> stores question, option, img, and answers 
+ * Control-> stores which question to display
+ */
 Scores = new Mongo.Collection('scores');
 Questions = new Mongo.Collection('questions');
-Answers = new Mongo.Collection('answers');
+Control = new Mongo.Collection('display_ctl');
 
+// client code
 if (Meteor.isClient) {
 
+	/*
+	 * obtain scores, which question to display and hide the img panel by default
+	 */
 	Meteor.subscribe('theScores');
-	Meteor.subscribe('theQuestions');
-	Meteor.subscribe('theAnswers');
+	Meteor.subscribe('display_ctl');
 	Session.set("imgState", "hidden");
 
+	/*
+	 * score board helpers
+	 */
 	Template.score_board.helpers({
 		'A_score': function() {
 			return Scores.findOne({ house: 'A' });
@@ -30,33 +41,41 @@ if (Meteor.isClient) {
 		}
 	});
 
+	/*
+	 * question panel helpers
+	 */
 	Template.question_board.helpers({
 		'question': function() {
+			// obtain question id of question set be display now and subscribe
+			var current_qid = Control.findOne({ control_id: 0 });
+			console.log(current_qid.question_id);
+			Meteor.subscribe('theQuestions', current_qid.question_id);
+
+			// load question from db
 			var question = Questions.findOne();
+
+			// check if the program has image attached, if not hide image panel
 			if(question.path=="")
-				Session.set("imgState", "hidden");
+				Session.set("imgState", "hidden");	/* reactive */
 			else
 				Session.set("imgState", "");
 
 			return question;
 		},
 		'img_state': function() {
-			return Session.get("imgState");
+			return Session.get("imgState");	/* reactive */
 		},
-		'answer': function() {
-			return Answers.findOne();
-		}
 	});
 }
 
+// server code
 if (Meteor.isServer) {
 	Meteor.startup(function () {
-		// code to run on server at startup
 
 		// drop collections to reset
 		Scores.remove({});
 		Questions.remove({});
-		Answers.remove({});
+		Control.remove({});
 
 		// initialize scores
 		Scores.insert({ house: 'A', score: 0 });
@@ -66,19 +85,32 @@ if (Meteor.isServer) {
 		Scores.insert({ house: 'L', score: 0 });
 		Scores.insert({ house: 'M', score: 0 });
 
-		// initialze questions
-		Questions.insert({ catalog: "catalog", Id: 1, content: "content", image: "", path: "" });
-		Answers.insert({ Id: 1, optionA: "Option A", optionB: "Option B", optionC: "Option C", optionD: "Option D", correct: "Correct Answer", state: "" });
+		// initialize display controller
+		Control.insert({ control_id: 0, question_id: { catalog: "A", group: 1, qid: 1 } });
 
+		// initialze questions
+		Questions.insert({ Id: { catalog: "A", group: 1, qid: 1 }, content: "content", image: "", path: "", optionA: "Option A", optionB: "Option B", optionC: "Option C", optionD: "Option D", correct: "Correct Answer", state: "" });
+		Questions.insert({ Id: { catalog: "B", group: 2, qid: 1 }, content: "content2", image: "", path: "", optionA: "Option A2", optionB: "Option B", optionC: "Option C", optionD: "Option D", correct: "Correct Answer", state: "" });
+
+	});
+
+	/*
+	 * publish scores and question id of question to be displayed
+	 */
+	Meteor.publish('display_ctl', function() {
+		return Control.find();
 	});
 
 	Meteor.publish('theScores', function() {
 		return Scores.find({},{ house: 1, score: 1 });
 	});
-	Meteor.publish('theAnswers', function() {
-		return Answers.find({});
-	});
-	Meteor.publish('theQuestions', function() {
-		return Questions.find({});
+
+
+	Meteor.publish('theQuestions', function(qid) {
+		var current_question = Control.findOne({ control_id: 0 });
+		console.log(qid.catalog+' '+qid.group+' '+qid.qid)
+		//if(current_question.question_id==qid) {
+			return Questions.find({ Id: { catalog: qid.catalog, group: qid.group, qid: qid.qid } });
+		//}
 	});
 }
